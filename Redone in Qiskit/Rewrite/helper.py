bb90f2d7a709d0c8bc423e9de0a8e633b6ccfc12
Kumar <swagat.kumar02@ntu.ac.uk>
2022-04-05 16:23:13 +0100

    put jupyter notebook code into Rewrite folder

---
+++ Redone in Qiskit/Rewrite/helper.py
@@ -0,0 +1,265 @@
+import numpy as np
+
+#-----------------#
+# General Helpers #
+#-----------------#
+
+def int_to_base(x, b, num_digits):
+    '''
+    convert a non-negative integer x to base b and return the digits in a list
+    '''
+    if x < 0:
+        raise ValueError('x must be non-negative')
+    digits = [0] * num_digits
+    for i in range(num_digits):
+        digits[i] = x % b
+        x = x//b
+    return digits
+
+def base_to_int(digits, b):
+    '''
+    convert a list of base b digits to an integer
+    '''
+    x = 0
+    for i in range(len(digits)):
+        x += digits[i] * (b**i)
+    return x
+
+#----------------------------#
+# Pauli/Sigma Matrix Related #
+#----------------------------#
+
+sigma_matrices = np.zeros([4,2,2], dtype=complex)
+# sigma_matrices is a list of 4x4 matrices, sigma_matrix[0] is I, etc up to Z
+sigma_matrices[0] = np.array([[1,0],[0,1]])
+sigma_matrices[1] = np.array([[0,1],[1,0]])
+sigma_matrices[2] = np.array([[0,-1j],[1j,0]])
+sigma_matrices[3] = np.array([[1,0],[0,-1]])
+
+# maps the product of the Pauli Matrices
+# sigma_i sigma_j = pauli_prod[1][i,j] * sigma_(pauli_prod[0][i,j])
+#                    ^ coefficient               ^ matrix identifier
+pauli_prod = [ np.zeros([4,4],dtype=int), np.zeros([4,4],dtype=complex) ]
+
+pauli_prod[0][0,:]=[0,1,2,3]
+pauli_prod[0][1,:]=[1,0,3,2]
+pauli_prod[0][2,:]=[2,3,0,1]
+pauli_prod[0][3,:]=[3,2,1,0]
+
+pauli_prod[1][0,:]=[1,  1,  1,  1]
+pauli_prod[1][1,:]=[1,  1, 1j,-1j]
+pauli_prod[1][2,:]=[1,-1j,  1, 1j]
+pauli_prod[1][3,:]=[1, 1j,-1j,  1]
+
+def pauli_string_prod(p1,p2,nbits):
+    '''
+    returns the product of two nbit long pauli strings
+    p1 and p2 are both nbit digit base 4 number representing the pauli string
+    
+    an nbit digit base 4 number representing the product, and the coefficient are returned
+    '''
+    pstring1 = int_to_base(p1,4,nbits)
+    pstring2 = int_to_base(p2,4,nbits)
+    
+    c = 1+0.j
+    prod = [0]*nbits
+    
+    for i in range(nbits):
+        prod[i] = pauli_prod[0][pstring1[i], pstring2[i]]
+        c      *= pauli_prod[1][pstring1[i], pstring2[i]]
+    
+    return base_to_int(prod,4), c
+
+def odd_y_pauli_strings(nbits):
+    '''
+    returns a list of all the nbit long pauli strings with an odd number of Ys
+    '''
+    
+    nops = 4**nbits
+    odd_y = []
+    for i in range(nops):
+        p = int_to_base(i,4,nbits)
+        num_y = 0
+        # count the number of Y gates in the string
+        for x in p:
+            if x == 2:
+                num_y += 1
+        # append if odd number of Ys
+        if num_y %2 == 1:
+            odd_y.append(i)
+    return odd_y
+
+#-------------------------#
+# Quantum Circuit Related #
+#-------------------------#
+
+def run_circuit(qc, backend, num_shots=1024):
+    '''
+    Run the circuit and return a dictionary of the measured counts
+
+    qc:         The circuit you are running
+    backend:    What backend you're running the circuit on
+    num_shots:  The number of times you run the circuit for measurement statistics
+    '''
+    result = execute(qc, backend, shots=num_shots).result()
+    return dict(result.get_counts())
+
+def measure(qc, p, qbits, backend, num_shots=1024):
+    '''
+    Measures in the pauli string P basis and returns the expected value from the statistics
+    assumes the quantum circuit has the same number of quantum and classial bits
+    '''
+    
+    # <psi|I|psi> = 1 for all states
+    if p == 0:
+        return 1
+    
+    nbits = len(qbits)
+    pstring = int_to_base(p, 4, nbits)
+
+    # Stores the qubit indices that aren't acted on by I
+    active = []
+
+    for i in range(nbits):
+        if pstring[i] == 0:
+            # I only has +1 eigenvalues, so the measurement is +1
+            continue
+        elif pstring[i] == 1:
+            # Apply H to rotate from X to Z basis
+            qc.h(qbits[i])
+        elif pstring[i] == 2:
+            # Apply Rx(-pi/2) to rotate from Y to Z basis
+            qc.rx(-np.pi/2, qbits[i])
+        elif pstring[i] == 3:
+            # Already in Z-basis
+            None
+        active.append( qbits[i] )
+
+    # Add measurements to the circuit
+    for bit in active:
+        qc.measure(bit,bit)
+
+    # Get the measurement statistics
+    counts = run_circuit(qc, backend, num_shots=num_shots)
+
+    expectation = 0
+    for key in counts.keys():
+        # Remove any spaces in the key
+        k = key.replace(' ','') 
+        # reverse the key to match ordering of bits
+        k = k[::-1]
+        sign = 1
+        # Every 1 measured is a -1 eigenvalue
+        for a in active:
+            if k[ a ] == '1':
+                sign *= -1
+        expectation += sign*counts[key]
+
+    expectation /= num_shots
+    return expectation, counts
+
+def pauli_string_exp(qc, qbits, p, theta):
+    '''
+    add gates to perform exp(-i theta/2 P) where P is the pauli string
+    '''
+    
+    nbits = len(qbits)
+    
+    pstring = int_to_base(p,4,nbits)
+    
+    # stores the qubit indices that aren't I
+    active = []
+    # stores the corresponding gate on the active qubit
+    gates  = []
+    for i in range(nbits):
+        if pstring[i] != 0:
+            active.append(qbits[i])
+            gates.append(pstring[i])
+    
+    nactive = len(active)
+    
+    # If the string is identity, the effect is to multiply by a global phase, so we can ignore it
+    if nactive == 0:
+        return
+    # If there is only one active qubit, we can rotate it directly
+    elif nactive == 1:
+        if gates[0] == 1:
+            qc.rx(theta, active[0])
+        elif gates[0] == 2:
+            qc.ry(theta, active[0])
+        elif gates[0] == 3:
+            qc.rz(theta, active[0])
+    # Apply the rotation about the Pauli string for multiple active qubits
+    else:
+        # Rotate to Z basis
+        for i in range(nactive):
+            # Apply H to go from X to Z basis
+            if gates[i] == 1:
+                qc.h(active[i])
+            # Apply Rx(pi/2) to go from Y to Z basis
+            elif gates[i] == 2:
+                qc.rx(np.pi/2, active[i])
+        
+        # Cascading CNOTs
+        for i in range(nactive-1):
+            qc.cx(active[i],active[i+1])
+            
+        # Rotate
+        qc.rz(theta, active[-1])
+        
+        # Undo cascading CNOTs
+        for i in range(nactive-2,-1,-1):
+            qc.cx(active[i],active[i+1])
+            
+        # Rotate back to original basis
+        for i in range(nactive):
+            # Apply H to go from Z to X basis
+            if gates[i] == 1:
+                qc.h(active[i])
+            # Apply Rx(-pi/2) to go from Z to Y basis
+            elif gates[i] == 2:
+                qc.rx(-np.pi/2,active[i])
+
+# --------------------#
+# Hamiltonian Related #
+# --------------------#
+def get_h_matrix(hm_list, nbits):
+    num_basis = 2**nbits
+    h_mat = np.zeros([num_basis,num_basis],dtype=complex)
+    
+    for hm in hm_list:
+        active = hm[2]
+        nactive = len(active)
+        nterms = len(hm[0])
+        # Loop through the Pauli terms in hm
+        for i in range(nterms):
+            full_pauli_str = [0] * nbits
+            partial_pauli_str = int_to_base(hm[0][i],4,nactive)
+            for j in range(len(active)):
+                full_pauli_str[active[j]] = partial_pauli_str[j]
+            
+            # The matrix for the term is a tensor product of the corresponding Pauli matrices
+            term_matrix = sigma_matrices[full_pauli_str[0]]
+            for j in range(1,nbits):
+                term_matrix = np.kron(term_matrix, sigma_matrices[full_pauli_str[j]])
+            # Scale by the coefficient of the term
+            term_matrix *= hm[1][i]
+            
+            # Add the term to the final matrix
+            h_mat += term_matrix
+    return h_mat
+
+def get_spectrum(hm_list, nbits):
+    '''
+    returns the spectrum of the Hamiltonian
+    '''
+    h_mat = get_h_matrix(hm_list, nbits)
+    return np.linalg.eig(h_mat)
+
+def get_gs(hm_list, nbits):
+    '''
+    returns the ground state energy and the ground state vector of the Hamiltonian
+    '''
+    w,v = get_spectrum(hm_list,nbits)
+    i = np.argmin(w)
+    return w[i],v[:,i]
---
+++ Redone in Qiskit/Rewrite/qite.py
@@ -0,0 +1,180 @@
+import numpy as np
+from qiskit import execute, Aer
+from qiskit import QuantumCircuit
+
+aer_sim = Aer.get_backend('aer_simulator')
+
+import time
+
+from helper import *
+
+def propogate(qc, alist):
+    '''
+    Applies the unitaries described by each time step's A
+    '''
+    for t in range(len(alist)):
+        active = alist[t][1]
+        nbits = len(active)
+        for pstring in range(1,4**nbits):
+            angle = np.real(alist[t][0][pstring])
+            if np.abs(angle) > 1e-5:
+                pauli_string_exp(qc, active, pstring, angle)
+    
+def update_alist(sigma_expectation, alist, db, delta, hm):
+    '''
+    updates the a term for hm given the tomography of the state prepared so far from sigma_expectation,
+    returns the norm of the imaginary time evolved state
+    '''
+    # number of qubits that the term acts on
+    nbits = len(hm[2])
+    # number of pauli terms present in hm
+    nterms = len(hm[0])
+    
+    nops = 4**nbits
+    
+    # Step 1: Obtain S matrix:
+    # S[I,J] = <psi| sigma_I sigma_J |psi>
+    
+    S = np.zeros([nops,nops],dtype=complex)
+    for i in range(nops):
+        for j in range(nops):
+            p,c_ = pauli_string_prod(i,j,nbits)
+            S[i,j] = sigma_expectation[p]*c_
+    
+    # Step 2: Obtain b vector
+    b = np.zeros(nops,dtype=complex)
+    c = 1
+    
+    for i in range(nterms):
+        c -= 2* db* hm[1][i] * sigma_expectation[hm[0][i]]
+    c = np.sqrt(c)
+    
+    for i in range(nops):
+        b[i] += ( 1/c - 1 ) * sigma_expectation[i] / db
+        for j in range(nterms):
+            p,c_ = pauli_string_prod(i,hm[0][j],nbits)
+            b[i] -= hm[1][j] * c_ * sigma_expectation[p] / c
+    b = -2*np.imag(b)
+    
+    # Step 3: Add regularizer to make sure the system can be solved
+    dalpha = np.eye(nops)*delta
+    
+    # Step 4: Solve the system
+    x = np.linalg.lstsq(2*np.real(S) + dalpha, -b, rcond=-1)[0]
+    
+    # Multiply by -2 db because the rotation gate is of the form exp(-i theta/2 P), but we want exp(i theta P)
+    a_coefficients = -2*db*x
+    # Append to the a coefficients
+    alist.append([a_coefficients, hm[2]])
+    
+    return c
+
+def measure_energy(alist, nbits, hm_list, backend, shots=1024):
+    '''
+    Measure the expected energy of the state evolved by alist, given the hm_list of the Hamiltonian
+    '''
+    Energy = 0
+    
+    # For each hm term
+    for hm in hm_list:
+        # For each pauli term in hm
+        for j in range(len(hm[0])):
+            qc = QuantumCircuit(nbits, nbits)
+            # Get the state evolved from alist
+            propogate(qc, alist)
+            
+            # Measure the expected value of the pauli term in this state
+            expectation, counts = measure(qc, hm[0][j], hm[2], backend, num_shots=shots)
+            
+            # Add this with the relevant weight to the energy total
+            Energy += hm[1][j] * expectation
+    return Energy
+
+def tomography(alist, qbits, nbits, backend, shots=1024):
+    '''
+    Perform tomography on the state evolved by alist
+    '''
+    nactive = len(qbits)
+    nops = 4**nactive
+    
+    sigma_expectation = np.zeros(nops,dtype=float)
+    
+    # <psi|I|psi> = 1 for all states
+    sigma_expectation[0] = 1
+    
+    for i in range(1,nops):
+        qc = QuantumCircuit(nbits,nbits)
+        # Get the state evolved from alist
+        propogate(qc,alist)
+        
+        # measure the expectation of the pauli string indexed by i
+        sigma_expectation[i],counts = measure(qc, i, qbits, backend, num_shots=shots)
+    return sigma_expectation
+
+def qite_step(alist, db, delta, nbits, hm_list, backend, shots=1024):
+    '''
+    performs one imaginary time step of size db in the QITE
+    '''
+    for hm in hm_list:
+        # peform tomography on the active qubits in hm
+        sigma_expectation = tomography(alist, hm[2], nbits, backend, shots)
+        # obtain the next alist term corresponding to hm
+        norm = update_alist(sigma_expectation, alist, db, delta, hm)
+    return alist
+
+def log_qite_run(db, delta, N, nbits, hm_list, alist, file_name='hm-alist'):
+    logging = []
+    logging.append(db)
+    logging.append(delta)
+    logging.append(N)
+    logging.append(nbits)
+    logging.append(hm_list)
+    logging.append(alist)
+    
+    np.save(file_name+'.npy',np.asarray(logging,dtype=object)) 
+
+def qite(db, delta, N, nbits, hm_list, backend, shots=1024, details=False, log=False, log_file = 'hm-alist'):
+    '''
+    Performs the QITE algorithm with: 
+        db: a time step,
+        delta: a regularization,
+        N: number of total iterations (time steps)
+        nbits: number of qubits in the whole system
+        hm_list: list of local terms in the Hamiltonian
+        backend: the backend that the quantum circuit will run on
+        shots: the number of shots for each circuit run
+        details: flag to show details while the algorithm runs
+    '''
+    # stores the mean energy for each time step
+    E = np.zeros(N+1,dtype=float)
+    # stores the iteration times
+    times = np.zeros(N+1,dtype=float)
+    
+    alist = []
+    
+    E[0] = measure_energy(alist, nbits, hm_list, backend, shots)
+    
+    start = 0
+    end = 0
+    if details:
+        print('Starting QITE Loop')
+    
+    for i in range(1,N+1):
+        if details:
+            print('i={}:'.format(i), end='  ', flush=True)
+            start = time.time()
+
+        # correction_matrix = estimate_assignment_probs()
+        alist = qite_step(alist, db, delta, nbits, hm_list, backend, shots)
+        # Record the energy of the system after the QITE step
+        E[i] = measure_energy(alist, nbits, hm_list, backend, shots)
+
+        if details:
+            end = time.time()
+            duration = end-start
+            times[i] = duration
+            print('Execution time: {:.2f} seconds'.format(duration))
+
+    if log:
+        log_qite_run(db, delta, N, nbits, hm_list, alist, log_file)
+    return E,times
