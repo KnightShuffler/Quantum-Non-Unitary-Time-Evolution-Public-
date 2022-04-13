b2c21c12c1e754b19fd6b60d48bda288df5e3fe0
Kumar <swagat.kumar02@ntu.ac.uk>
2022-04-13 11:53:37 +0100

    fixed propagate typo and added python file for odd_y qite

--- Redone in Qiskit/Rewrite/y_qite.py
+++ Redone in Qiskit/Rewrite/y_qite.py
@@ -8,68 +8,87 @@ import time
 
 from helper import *
 
-def propagate(qc, alist):
-    '''
-    Applies the unitaries described by each time step's A
-    '''
+def y_propagate(qc, alist):
     for t in range(len(alist)):
         active = alist[t][1]
         nbits = len(active)
-        for pstring in range(1,4**nbits):
-            angle = np.real(alist[t][0][pstring])
+        odd_y_paulis = odd_y_pauli_strings(nbits)
+        for i in range(len(odd_y_paulis)):
+            angle = np.real(alist[t][0][i])
             if np.abs(angle) > 1e-5:
-                pauli_string_exp(qc, active, pstring, angle)
+                pauli_string_exp(qc, active, odd_y_paulis[i], angle)
+
+def y_tomography(hm, alist, qbits, nbits, backend, shots=1024):
+    nactive = len(qbits)
+    odd_ys = odd_y_pauli_strings(nactive)
+    nops = len(odd_ys)
     
-def update_alist(sigma_expectation, alist, db, delta, hm):
-    '''
-    updates the a term for hm given the tomography of the state prepared so far from sigma_expectation,
-    returns the norm of the imaginary time evolved state
-    '''
-    # number of qubits that the term acts on
-    nbits = len(hm[2])
-    # number of pauli terms present in hm
+    keys = []
+    
+    for j in hm[0]:
+        if not(j in keys):
+            keys.append(j)
+    
+    for i in odd_ys:
+        for j in odd_ys:
+            p,c = pauli_string_prod(i,j,nactive)
+            if not (p in keys):
+                keys.append(p)
+        for j in hm[0]:
+            p,c = pauli_string_prod(i,j,nactive)
+            if not (p in keys):
+                keys.append(p)
+    
+#     print(keys)
+    sigma_expectation = {}
+    for key in keys:
+#         sigma_expectation[key] = 0.0j
+        qc = QuantumCircuit(nbits, nbits)
+        y_propagate(qc, alist)
+        sigma_expectation[key],counts = measure(qc, key, qbits, backend, shots)
+    
+    return sigma_expectation
+
+def y_update_alist(sigma_expectation, alist, db, delta, hm):
+    nactive = len(hm[2])
     nterms = len(hm[0])
     
-    nops = 4**nbits
-    
-    # Step 1: Obtain S matrix:
-    # S[I,J] = <psi| sigma_I sigma_J |psi>
+    odd_ys = odd_y_pauli_strings(nactive)
+    nops = len(odd_ys)
     
     S = np.zeros([nops,nops],dtype=complex)
     for i in range(nops):
         for j in range(nops):
-            p,c_ = pauli_string_prod(i,j,nbits)
-            S[i,j] = sigma_expectation[p]*c_
+            p,c_ = pauli_string_prod(odd_ys[i], odd_ys[j], nactive)
+            S[i,j] = sigma_expectation[p] * c_
     
-    # Step 2: Obtain b vector
-    b = np.zeros(nops,dtype=complex)
+    b = np.zeros(nops, dtype=complex)
     c = 1
     
     for i in range(nterms):
-        c -= 2* db* hm[1][i] * sigma_expectation[hm[0][i]]
+        c -= 2*db*sigma_expectation[hm[0][i]]
     c = np.sqrt(c)
     
-    for i in range(nops):
-        b[i] += ( 1/c - 1 ) * sigma_expectation[i] / db
+    
+    for k in range(len(odd_ys)):
+        # This part not needed since it will always be real
+#         b[k] += (1/c - 1) * sigma_expectation[odd_ys[k]]
         for j in range(nterms):
-            p,c_ = pauli_string_prod(i,hm[0][j],nbits)
-            b[i] -= hm[1][j] * c_ * sigma_expectation[p] / c
+            p,c_ = pauli_string_prod(odd_ys[k], hm[0][j], nactive)
+            b[k] -= hm[1][j] * c_ * sigma_expectation[p] / c
     b = -2*np.imag(b)
     
-    # Step 3: Add regularizer to make sure the system can be solved
-    dalpha = np.eye(nops)*delta
+    dalpha = np.eye(nops) * delta
     
-    # Step 4: Solve the system
     x = np.linalg.lstsq(2*np.real(S) + dalpha, -b, rcond=-1)[0]
     
-    # Multiply by -2 db because the rotation gate is of the form exp(-i theta/2 P), but we want exp(i theta P)
     a_coefficients = -2*db*x
-    # Append to the a coefficients
+    
     alist.append([a_coefficients, hm[2]])
     
     return c
 
-def measure_energy(alist, nbits, hm_list, backend, shots=1024):
+def y_measure_energy(alist, nbits, hm_list, backend, shots=1024):
     '''
     Measure the expected energy of the state evolved by alist, given the hm_list of the Hamiltonian
     '''
@@ -81,7 +100,7 @@ def measure_energy(alist, nbits, hm_list, backend, shots=1024):
         for j in range(len(hm[0])):
             qc = QuantumCircuit(nbits, nbits)
             # Get the state evolved from alist
-            propagate(qc, alist)
+            y_propagate(qc, alist)
             
             # Measure the expected value of the pauli term in this state
             expectation, counts = measure(qc, hm[0][j], hm[2], backend, num_shots=shots)
@@ -90,41 +109,20 @@ def measure_energy(alist, nbits, hm_list, backend, shots=1024):
             Energy += hm[1][j] * expectation
     return Energy
 
-def tomography(alist, qbits, nbits, backend, shots=1024):
-    '''
-    Perform tomography on the state evolved by alist
-    '''
-    nactive = len(qbits)
-    nops = 4**nactive
-    
-    sigma_expectation = np.zeros(nops,dtype=float)
-    
-    # <psi|I|psi> = 1 for all states
-    sigma_expectation[0] = 1
-    
-    for i in range(1,nops):
-        qc = QuantumCircuit(nbits,nbits)
-        # Get the state evolved from alist
-        propagate(qc,alist)
-        
-        # measure the expectation of the pauli string indexed by i
-        sigma_expectation[i],counts = measure(qc, i, qbits, backend, num_shots=shots)
-    return sigma_expectation
-
-def qite_step(alist, db, delta, nbits, hm_list, backend, shots=1024):
+def y_qite_step(alist, db, delta, nbits, hm_list, backend, shots=1024):
     '''
     performs one imaginary time step of size db in the QITE
     '''
     for hm in hm_list:
         # peform tomography on the active qubits in hm
-        sigma_expectation = tomography(alist, hm[2], nbits, backend, shots)
+        sigma_expectation = y_tomography(hm, alist, hm[2], nbits, backend, shots=1024)
         # obtain the next alist term corresponding to hm
-        norm = update_alist(sigma_expectation, alist, db, delta, hm)
+        norm = y_update_alist(sigma_expectation, alist, db, delta, hm)
     return alist
 
-def log_qite_run(db, delta, N, nbits, hm_list, alist, file_name='hm-alist'):
+def y_log_qite_run(db, delta, N, nbits, hm_list, alist, file_name='y_hm-alist'):
     logging = []
-    logging.append(False)
+    logging.append(True)
     logging.append(db)
     logging.append(delta)
     logging.append(N)
@@ -132,9 +130,9 @@ def log_qite_run(db, delta, N, nbits, hm_list, alist, file_name='hm-alist'):
     logging.append(hm_list)
     logging.append(alist)
     
-    np.save(file_name+'.npy',np.asarray(logging,dtype=object)) 
+    np.save(file_name+'.npy',np.asarray(logging,dtype=object))
 
-def qite(db, delta, N, nbits, hm_list, backend, shots=1024, details=False, log=False, log_file = 'hm-alist'):
+def y_qite(db, delta, N, nbits, hm_list, backend, shots=1024, details=False, log=False, log_file = 'y_hm-alist'):
     '''
     Performs the QITE algorithm with: 
         db: a time step,
@@ -153,7 +151,7 @@ def qite(db, delta, N, nbits, hm_list, backend, shots=1024, details=False, log=F
     
     alist = []
     
-    E[0] = measure_energy(alist, nbits, hm_list, backend, shots)
+    E[0] = y_measure_energy(alist, nbits, hm_list, backend, shots)
     
     start = 0
     end = 0
@@ -166,9 +164,9 @@ def qite(db, delta, N, nbits, hm_list, backend, shots=1024, details=False, log=F
             start = time.time()
 
         # correction_matrix = estimate_assignment_probs()
-        alist = qite_step(alist, db, delta, nbits, hm_list, backend, shots)
+        alist = y_qite_step(alist, db, delta, nbits, hm_list, backend, shots)
         # Record the energy of the system after the QITE step
-        E[i] = measure_energy(alist, nbits, hm_list, backend, shots)
+        E[i] = y_measure_energy(alist, nbits, hm_list, backend, shots)
 
         if details:
             end = time.time()
@@ -177,5 +175,5 @@ def qite(db, delta, N, nbits, hm_list, backend, shots=1024, details=False, log=F
             print('Execution time: {:.2f} seconds'.format(duration))
 
     if log:
-        log_qite_run(db, delta, N, nbits, hm_list, alist, log_file)
+        y_log_qite_run(db, delta, N, nbits, hm_list, alist, log_file)
     return E,times
