557eae7e776a5bafca2f500f0a10e12d00000719
Swagat Kumar <swagat.ks.97@gmail.com>
2022-05-11 12:48:28 +0100

    removed original motta files

--- mettscode/__init__.py
+++
@@ -1,2 +0,0 @@
-# Author:        Chong Sun  <chongs0419@gmail.com>
-# METTS algorithm to calculate finite temperature properties
--- numerics/maxcut/6_vertices/R_2.5/dt_0.01/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --qos=parallel
-#SBATCH --partition=parallel
-#SBATCH -N 1
-#SBATCH -t 12:00:00
-#SBATCH --mem=256000
-
-export MKL_NUM_THREADS=28
-srun python test.py > test.out
