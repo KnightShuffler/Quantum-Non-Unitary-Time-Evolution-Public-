b984680d3d31fafd81d843fd1fef13ab14679f88
Swagat_work <swagat.kumar02@ntu.ac.uk>
2023-02-15 13:48:36 +0000

    added an output class to store the output of qnute experiments

---
+++ qnute_output.py
@@ -0,0 +1,18 @@
+import numpy as np
+from qnute_params import QNUTE_params
+
+class QNUTE_output:
+    def __init__(self, params: QNUTE_params):
+        self.times = np.zeros(params.N)
+        self.a_list = []
+        self.S_list = []
+        self.b_list = []
+        self.c_list = []
+        if params.store_state_vector:
+            self.svs = np.zeros((params.N+1, 2**params.nbits),dtype=complex)
+        else:
+            self.svs = None
+        
+        self.measurements = {}
+        for m in params.objective_measurements:
+            self.measurements[m[0]] = np.zeros(params.N+1,dtype=float)
