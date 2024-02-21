bc87415facfad546817c043519bd8deb938c5257
Swagat Laptop <swagat.ks.97@gmail.com>
2024-02-21 11:01:21 +0000

    updated test to calculate evolution of simple sine in the heat equation

--- tests/heat_eqn/run_test.py
+++ tests/heat_eqn/run_test.py
@@ -1,52 +1,94 @@
 import numpy as np
+from numba import njit
 from qnute.hamiltonian import Hamiltonian
 from qnute.hamiltonian.laplacian import generateLaplaceHamiltonian1D
+from qnute.simulation.numerical_sim import qnute
+from qnute.simulation.numerical_sim import get_theoretical_evolution as get_qnute_th_evolution
+from qnute.simulation.parameters import QNUTE_params as Params
+from qnute.simulation.output import QNUTE_output as Output
+import matplotlib.pyplot as plt
+
+import logging
+
+@njit
+def get_theoretical_evolution(L:float,Nx:int,dx:float,Nt:int,dt:float):
+    theoretical_solution = np.zeros((Nt+1,Nx), dtype=np.float64)
+    x = np.arange(Nx)*dx + dx
+    for i,t in enumerate(np.arange(Nt+1)*dt):
+        theoretical_solution[i,:] = np.sin(np.pi*x/L) * np.exp(-np.power(np.pi/L,2)*t)
+    return theoretical_solution
 
 def main():
-    # hml1 = [
-    #     [np.array([5, 10],dtype=np.uint32), np.array([0.1, 0.1],dtype=np.complex128), [(2,),(3,)]],
-    #     [np.array([3], dtype=np.uint32), np.array([0.01],dtype=np.complex128), [(2,)]]
-    # ]
-
-    # hml2 = [
-    #     [np.array([15, 6],dtype=np.uint32), np.array([0.1, 0.1],dtype=np.complex128), [(0,),(1,)]],
-    #     [np.array([2],dtype=np.uint32), np.array([0.01,],dtype=np.complex128), [(0,)]]
-    # ]
-    n = 3
+    logging.getLogger().setLevel(logging.INFO)
+    n = 4
     qubit_map = {(i,):i for i in range(n)}
 
-    # hml12 = hm_list_tensor(hml1, hml2)
-    # hml21 = hm_list_tensor(hml2, hml1)
+    Nx = 2**n
+    # L = 1.0
+    # dx = L / (Nx+1)
+    dx = 0.1
+    L = dx * (Nx+1)
+    T = 1.0
 
-    # H1 = Hamiltonian(hml1,qubit_map)
-    # H2 = Hamiltonian(hml2,qubit_map)
-    # H1_H2 = Hamiltonian(hml12, qubit_map)
-    # H2_H1 = Hamiltonian(hml21, qubit_map)
+    dtau = 0.1
+    Ntau = np.int32(np.ceil(T / dtau))
+    dt = dtau*dx*dx
+    Nt = np.int32(np.ceil(T/dt))
 
-    # print(H1)
-    # print(H2)
-    # print(H1_H2)
-    # print(H2_H1)
+    print(f'Nt = {Nt}, Ntau = {Ntau}')
 
-    H = generateLaplaceHamiltonian1D(n, 0.1, False, False)
+    D = 2
+    delta = 0.1
+    num_shots=0
+    backend=None
+    trotter_flag = True
+
+    H = generateLaplaceHamiltonian1D(n, 1.0, False, False)
     print(H)
     print(H.pterm_list)
-    print(H.hm_indices)
+    # print(H.hm_indices)
     print(np.real(H.get_matrix()))
 
-    # hml1 = [
-    #     [np.array([0],dtype=np.uint32), np.array([1.0+0.j],dtype=np.complex128), [1]],
-    #     [np.array([3],dtype=np.uint32), np.array([1.0+0.j],dtype=np.complex128), [2]]
-    # ]
+    # print(H.get_hm_pterms(0))
+    # norm = np.linalg.norm(H.pterm_list['amplitude'])
+    # print(f"norm = {norm:0.5f}\n")
+    # print(np.real(H.get_matrix())/norm)
 
-    # hml2 = [
-    #     [np.array([1,2],dtype=np.uint32), np.array([0.5,0.5],dtype=np.complex128), [0]]
-    # ]
+    times = np.arange(Nt+1)*dt
+    x = np.arange(Nx)*dx + dx
+    theoretical_solution = get_theoretical_evolution(L,Nx,dx,Nt,dt)
 
-    # H1 = Hamiltonian(hml1, 3)
-    # H2 = Hamiltonian(hml2, 1)
+    psi0 = theoretical_solution[0,:]
+    c0 = np.linalg.norm(psi0)
+    psi0 /= c0
 
-    # print(Hamiltonian.tensor_product(H1,H2))
+    params = Params(H, 1, n, qubit_map)
+    params.load_hamiltonian_params(D, False, True)
+    params.set_run_params(dtau, delta, Nt, num_shots, backend, init_sv=psi0,trotter_flag=trotter_flag)
+
+    out = qnute(params,log_frequency=1,c0=c0)
+    print(len(out.c_list))
+
+    # qnute_svs = get_qnute_th_evolution(np.real(H.get_matrix()), psi0, dtau, Ntau)
+
+    fig,axs = plt.subplots(1,2,sharex=True,sharey=True,figsize=(8,4))
+
+    for i,t in enumerate(times):
+        l, =axs[0].plot(x, theoretical_solution[i,:],label=f't={t:0.3f}')
+    taus = np.arange(Ntau+1)*dtau
+    for i,t in enumerate(times):
+        axs[1].plot(x,np.real(out.svs[i,:])*np.prod(out.c_list[0:i+1]), label=f'tau={t:0.3f}')
+        # axs[1].plot(x,qnute_svs[i,:], label=f'tau={t:0.3f}')
+
+    axs[0].set_title('Theoretical Evolution')
+    axs[1].set_title('QNUTE Evolution')
+    for ax in axs:
+        ax.grid(True)
+    plt.xlim(-0.01,L + 0.01)
+    plt.xticks(np.arange(dx,(Nx+1)*dx,dx))
+    plt.ylim(-0.01, 1.01)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
