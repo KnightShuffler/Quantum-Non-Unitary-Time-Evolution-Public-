464ac49729176b0092d18e97e1ab78dc9a826d0d
Swagat Laptop <swagat.ks.97@gmail.com>
2024-03-26 11:08:18 +0000

    updated laplacian for no offset

--- qnute/hamiltonian/laplacian.py
+++ qnute/hamiltonian/laplacian.py
@@ -1,65 +1,70 @@
 import numpy as np
 from functools import lru_cache
 
-from .import Hamiltonian, hm_list_sum, hm_list_tensor
+from .import Hamiltonian, hm_list_sum, hm_list_tensor, get_identity_hm_list
 
-laplaceKernel_hm_list = [
+laplaceKernel_hm_list = [[
     np.array([1,0], dtype=np.uint32),
     np.array([1.0, -2.0], dtype=np.complex128),
-]
+    np.array([0],dtype=np.int32)
+]]
 
-lowerLeftKernel_hm_list = [
+lowerLeftKernel_hm_list = [[
     np.array([1,2],dtype=np.uint32),
-    np.array([0.5, -0.5j], dtype=np.complex128)
-]
+    np.array([0.5, -0.5j], dtype=np.complex128),
+    np.array([0],dtype=np.int32)
+]]
 
-upperRightKernel_hm_list = [
+upperRightKernel_hm_list = [[
     np.array([1,2],dtype=np.uint32),
-    np.array([0.5, 0.5j], dtype=np.complex128)
-]
+    np.array([0.5, 0.5j], dtype=np.complex128),
+    np.array([0],dtype=np.int32)
+]]
 
-upperLeftKernel_hm_list = [
+upperLeftKernel_hm_list = [[
     np.array([3,0],dtype=np.uint32),
-    np.array([0.5,0.5],dtype=np.complex128)
-]
+    np.array([0.5,0.5],dtype=np.complex128),
+    np.array([0],dtype=np.int32)
+]]
 
-lowerRightKernel_hm_list = [
+lowerRightKernel_hm_list = [[
     np.array([3,0],dtype=np.uint32),
-    np.array([-0.5,0.5],dtype=np.complex128)
-]
+    np.array([-0.5,0.5],dtype=np.complex128),
+    np.array([0],dtype=np.int32)
+]]
 
 @lru_cache
 def get_lowerLeft_hm_list(num_qbits:int):
     if num_qbits == 1:
-        return [[lowerLeftKernel_hm_list[0], lowerLeftKernel_hm_list[1], np.array([0])]]
-    return hm_list_tensor([[lowerLeftKernel_hm_list[0], lowerLeftKernel_hm_list[1], np.array([0])]], get_lowerLeft_hm_list(num_qbits-1))
+        return lowerLeftKernel_hm_list
+    return hm_list_tensor(lowerLeftKernel_hm_list, get_lowerLeft_hm_list(num_qbits-1), 1, num_qbits-1)
 
 @lru_cache
 def get_lowerRight_hm_list(num_qbits:int):
     if num_qbits == 1:
-        return [[lowerRightKernel_hm_list[0], lowerRightKernel_hm_list[1], np.array([0])]]
-    return hm_list_tensor([[lowerRightKernel_hm_list[0], lowerRightKernel_hm_list[1], np.array([0])]], get_lowerRight_hm_list(num_qbits-1))
+        return lowerRightKernel_hm_list
+    return hm_list_tensor(lowerRightKernel_hm_list, get_lowerRight_hm_list(num_qbits-1), 1, num_qbits-1)
 
 @lru_cache
 def get_upperRight_hm_list(num_qbits:int):
     if num_qbits == 1:
-        return [[upperRightKernel_hm_list[0], upperRightKernel_hm_list[1], np.array([0])]]
-    return hm_list_tensor([[upperRightKernel_hm_list[0], upperRightKernel_hm_list[1], np.array([0])]], get_upperRight_hm_list(num_qbits-1))
+        return upperRightKernel_hm_list
+    return hm_list_tensor(upperRightKernel_hm_list, get_upperRight_hm_list(num_qbits-1), 1, num_qbits-1)
 
 @lru_cache
 def get_upperLeft_hm_list(num_qbits:int):
     if num_qbits == 1:
-        return [[upperLeftKernel_hm_list[0], upperLeftKernel_hm_list[1], np.array([0])]]
-    return hm_list_tensor([[upperLeftKernel_hm_list[0], upperLeftKernel_hm_list[1], np.array([0])]], get_upperLeft_hm_list(num_qbits-1))
+        return upperLeftKernel_hm_list
+    return hm_list_tensor(upperLeftKernel_hm_list, get_upperLeft_hm_list(num_qbits-1), 1, num_qbits-1)
 
 @lru_cache
 def get_laplace1D_hm_list(num_qbits:int):
     if num_qbits == 1:
-        return [[laplaceKernel_hm_list[0], laplaceKernel_hm_list[1], np.array([0])]]
+        return laplaceKernel_hm_list
     return hm_list_sum(
-        hm_list_tensor([[np.zeros(1,dtype=np.uint32), np.array([1.0],dtype=np.complex128), np.array([0])]], get_laplace1D_hm_list(num_qbits-1)),
-        hm_list_tensor(get_lowerLeft_hm_list(1), get_upperRight_hm_list(num_qbits-1)),
-        hm_list_tensor(get_upperRight_hm_list(1), get_lowerLeft_hm_list(num_qbits-1))
+        hm_list_tensor(get_identity_hm_list(1),   get_laplace1D_hm_list(num_qbits-1),  1, num_qbits-1),
+        hm_list_tensor(get_lowerLeft_hm_list(1),  get_upperRight_hm_list(num_qbits-1), 1, num_qbits-1),
+        hm_list_tensor(get_upperRight_hm_list(1), get_lowerLeft_hm_list(num_qbits-1),  1, num_qbits-1)
     )
 
 @lru_cache
@@ -74,30 +79,27 @@ def get_laplace1D_periodic_hm_list(num_qbits:int):
 def get_graycode_laplace1D_continuity_term(num_qbits:int):
     assert num_qbits >= 2
     if num_qbits == 2:
-        return hm_list_tensor([[np.array([1],dtype=np.uint32), np.array([1.0],dtype=np.complex128), np.array([0])]],
-                              get_lowerRight_hm_list(1))
+        return hm_list_tensor(get_identity_hm_list(1),get_lowerRight_hm_list(1), 1, 1)
     return hm_list_tensor(
-        [[np.array([1],dtype=np.uint32), np.array([1.0],dtype=np.complex128), np.array([0])]],
-        hm_list_tensor(get_lowerRight_hm_list(1), get_upperLeft_hm_list(num_qbits-2))
+        get_identity_hm_list(1),
+        hm_list_tensor(get_lowerRight_hm_list(1), get_upperLeft_hm_list(num_qbits-2), 1, num_qbits-2),
+        1, num_qbits-1
     )
 
 @lru_cache
 def get_graycode_laplace1D_periodic_term(num_qbits:int):
     # assert num_qbits >= 2
     # if num_qbits == 2:
-    #     return hm_list_tensor([[np.array([1],dtype=np.uint32), np.array([1.0],dtype=np.complex128), np.array([0])]],
-    #                           get_upperLeft_hm_list(1))
-    return hm_list_tensor(
-        [[np.array([1],dtype=np.uint32), np.array([1.0],dtype=np.complex128), np.array([0])]],
-        get_upperLeft_hm_list(num_qbits-1)
-    )
+    #     return hm_list_tensor(get_identity_hm_list(1),
+    #                           get_upperLeft_hm_list(1), 1, 1)
+    return hm_list_tensor(get_identity_hm_list(1), get_upperLeft_hm_list(num_qbits-1), 1, num_qbits-1)
 
 @lru_cache
 def get_graycode_laplace1D_hm_list(num_qbits:int):
     if num_qbits == 1:
-        return [[laplaceKernel_hm_list[0], laplaceKernel_hm_list[1], np.array([0])]]
+        return get_identity_hm_list(1)
     return hm_list_sum(
-        hm_list_tensor([[np.zeros(1,dtype=np.uint32), np.array([1.0],dtype=np.complex128), np.array([0])]], get_graycode_laplace1D_hm_list(num_qbits-1)),
+        hm_list_tensor(get_identity_hm_list(1), get_graycode_laplace1D_hm_list(num_qbits-1), 1, num_qbits-1),
         get_graycode_laplace1D_continuity_term(num_qbits)
     )
 
@@ -120,13 +122,11 @@ def generateLaplaceHamiltonian1D(num_qbits:int,
             hm_list = get_laplace1D_periodic_hm_list(num_qbits)
     else:
         if not periodic_bc_flag:
-            hm_list = hm_list_tensor([[upperLeftKernel_hm_list[0], upperLeftKernel_hm_list[1], np.array([0])]],
-                                 get_laplace1D_hm_list(num_qbits-1))
+            hm_list = hm_list_tensor(upperLeftKernel_hm_list, get_laplace1D_hm_list(num_qbits-1), 1, num_qbits-1)
             # hm_list = hm_list_tensor(get_laplace1D_hm_list(num_qbits-1),
             #                          [[upperLeftKernel_hm_list[0], upperLeftKernel_hm_list[1], [0]]])
         else:
-            hm_list = hm_list_tensor([[upperLeftKernel_hm_list[0], upperLeftKernel_hm_list[1], np.array([0])]],
-                                 get_laplace1D_periodic_hm_list(num_qbits-1))
+            hm_list = hm_list_tensor(upperLeftKernel_hm_list, get_laplace1D_periodic_hm_list(num_qbits-1), 1, num_qbits-1)
             # hm_list = hm_list_tensor(get_laplace1D_periodic_hm_list(num_qbits-1),
             #                          [[upperLeftKernel_hm_list[0], upperLeftKernel_hm_list[1], [0]]])
 
@@ -147,11 +147,11 @@ def generateGrayCodeLaplacian1D(num_qbits:int,
             hm_list = get_graycode_laplace1D_periodic_hm_list(num_qbits)
     else:
         if not periodic_bc_flag:
-            hm_list = hm_list_tensor([[upperLeftKernel_hm_list[0], upperLeftKernel_hm_list[1], [num_qbits-1]]],
-                                 get_graycode_laplace1D_hm_list(num_qbits-1))
+            hm_list = hm_list_tensor(upperLeftKernel_hm_list,
+                                 get_graycode_laplace1D_hm_list(num_qbits-1), 1, num_qbits-1)
         else:
-            hm_list = hm_list_tensor([[upperLeftKernel_hm_list[0], upperLeftKernel_hm_list[1], [num_qbits-1]]],
-                                 get_graycode_laplace1D_periodic_hm_list(num_qbits-1))
+            hm_list = hm_list_tensor(upperLeftKernel_hm_list,
+                                 get_graycode_laplace1D_periodic_hm_list(num_qbits-1), 1, num_qbits-1)
     
     for hm in hm_list:
         for i,amp in enumerate(hm[1]):
