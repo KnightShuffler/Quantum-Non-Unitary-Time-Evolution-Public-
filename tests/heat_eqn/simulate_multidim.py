ee2e6e1773be4922eab031f2e6d2a62edcdba7e2
Swagat Laptop <swagat.ks.97@gmail.com>
2024-03-28 15:51:01 +0000

    added multidimensional simulation code

--- tests/heat_eqn/simulate_multidim.py
+++ tests/heat_eqn/simulate_multidim.py
@@ -3,7 +3,7 @@ from numba import njit
 
 import logging 
 
-from qnute.hamiltonian.laplacian import generateLaplaceHamiltonian1D
+from qnute.hamiltonian.laplacian import generateLaplacianHamiltonianMultiDim
 from qnute.simulation.numerical_sim import qnute
 from qnute.simulation.parameters import QNUTE_params as Params
 
@@ -97,5 +97,47 @@ def get_analytical_solution(fourier_amplitudes:np.ndarray[float],
     return analytical_sols
 
 
-def run_heat_eqn_simulation(expt:ExperimentInput) -> np.ndarray[complex]:
-    pass
+def run_heat_eqn_simulation(expt:ExperimentInput, independent_cover_flag:bool=True) -> np.ndarray[float]:
+    min_dx = np.min(expt.dx)
+    dt = expt.dtau * min_dx**2
+    Nt = np.int32(np.ceil(expt.T / dt))
+    total_qbits = np.sum(expt.num_qbits)
+    N = 2**(total_qbits)
+
+    solutions = np.zeros((numD:=expt.D_list.shape[0], Nt+1, N), dtype=np.complex128)
+
+    H = generateLaplacianHamiltonianMultiDim(expt.num_qbits, expt.dx, expt.periodic_bc_flag)
+    H.multiply_scalar(expt.alpha)
+
+    c0 = np.linalg.norm(expt.f0)
+    psi0 = expt.f0 / c0
+
+    qubit_map = {(i,):i for i in range(np.sum(expt.num_qbits))}
+
+    params = Params(H, 1, total_qbits, qubit_map)
+
+    for Di,D in enumerate(expt.D_list):
+        if not independent_cover_flag:
+            if D < total_qbits:
+                u_domains = [list(range(i,i+D)) for i in range(total_qbits-D+1)]
+            else:
+                u_domains = [list(range(total_qbits))]
+        else:
+            u_domains = []
+            for j in range(expt.num_space_dims):
+                start = expt.num_qbits[j-1] if j > 0 else 0
+                if D < expt.num_qbits[j]:
+                    for i in range(expt.num_qbits[j]-D+1):
+                        u_domains.append(list(range(i+start,i+start+D)))
+                else:
+                    u_domains.append(list(range(start, start + expt.num_qbits[j])))
+
+        params.load_hamiltonian_params(D, u_domains, True, True)
+        params.set_run_params(expt.dtau, 0.1, Nt, 0, None, init_sv=psi0, trotter_flag=True)
+
+        out = qnute(params, log_frequency=100, c0=c0)
+        solutions[Di,:,:] = out.svs
+        for ti in range(Nt+1):
+            solutions[Di,ti,:] *= np.prod(out.c_list[0:ti+1])
+
+    return solutions.real
