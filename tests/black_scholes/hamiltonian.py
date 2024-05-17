53b9140d008ba6d6479620b25221df3ed2fbe530
Swagat Laptop <swagat.ks.97@gmail.com>
2024-05-17 10:58:52 +0100

    updated hamiltonian generation method

--- tests/black_scholes/hamiltonian.py
+++ tests/black_scholes/hamiltonian.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from qnute.hamiltonian import Hamiltonian, hm_list_tensor
-from qnute.hamiltonian.construction import get_lowerLeft_hm_list, get_lowerRight_hm_list
+from qnute.hamiltonian.construction import get_lowerLeft_hm_list, get_lowerRight_hm_list, get_upperLeft_hm_list, get_upperRight_hm_list
 from qnute.hamiltonian.examples.finite_difference.first_derivative import generateFirstDerivativeHamiltonian1D
 from qnute.hamiltonian.examples.finite_difference.laplacian import generateLaplaceHamiltonian1D
 from qnute.hamiltonian.examples.position_operator import generatePositionHamiltonian
@@ -23,16 +23,26 @@ def lowerRight2Ham(num_qbits:int) -> Hamiltonian:
                                       hm_list_tensor(get_lowerLeft_hm_list(1),
                                                      get_lowerRight_hm_list(1)) ), num_qbits)
 
+def upperLeftHam(num_qbits:int)->Hamiltonian:
+    return Hamiltonian(get_upperLeft_hm_list(num_qbits), num_qbits)
+def upperLeft1Ham(num_qbits:int)->Hamiltonian:
+    assert num_qbits >= 2
+    if num_qbits == 2:
+        return Hamiltonian(hm_list_tensor(get_upperLeft_hm_list(1), 
+                                          get_upperRight_hm_list(1)), num_qbits)
+    return Hamiltonian(hm_list_tensor(get_upperLeft_hm_list(num_qbits-1),
+                                      get_upperRight_hm_list(1)), num_qbits)
+
 def generateBlackScholesHamiltonian(bs_data:BlackScholesInfo,
                                     num_qbits:int
                                     )->Hamiltonian:
     assert num_qbits >= 2, 'Black Scholes Hamiltonian requries at least two qubits!'
     N = 2**num_qbits
-    dS = bs_data.Smax / (N-1)
+    dS = (bs_data.Smax-bs_data.Smin) / (N-1)
 
     if bs_data.basis == Basis.S:
         BSHam = (
-            (SHam:=generatePositionHamiltonian(num_qbits, 0, dS)) * generateFirstDerivativeHamiltonian1D(num_qbits, dS) * -(bs_data.r-bs_data.q) 
+            (SHam:=generatePositionHamiltonian(num_qbits, bs_data.Smin, dS)) * generateFirstDerivativeHamiltonian1D(num_qbits, dS) * -(bs_data.r-bs_data.q) 
             + SHam*SHam*generateLaplaceHamiltonian1D(num_qbits, dS)*(-(bs_data.sigma**2)/2) 
             + Hamiltonian.Identity(num_qbits)*(bs_data.r)
             )
@@ -52,6 +62,18 @@ def generateBlackScholesHamiltonian(bs_data:BlackScholesInfo,
                 BC_Ham = lowerRightHam(num_qbits) * ((bs_data.sigma*bs_data.Smax/dS)**2*(-5/2) - (bs_data.r-bs_data.q)*bs_data.Smax/dS)
                 BC_Ham += lowerRight1Ham(num_qbits) * ((bs_data.sigma*bs_data.Smax/dS)**2 *(3/2) + (bs_data.r-bs_data.q)*bs_data.Smax/(2*dS))
                 BC_Ham += lowerRight2Ham(num_qbits) * ((bs_data.sigma*bs_data.Smax/dS)**2/2)
+                BSHam += BC_Ham
+            case BoundaryConditions.DOUBLE_DIRICHLET_NODE:
+                BC_Ham = lowerRightHam(num_qbits) * -(bs_data.r + (bs_data.sigma*bs_data.Smax/dS)**2)
+                BC_Ham += lowerRight1Ham(num_qbits) * ((bs_data.sigma*bs_data.Smax/dS)**2 / 2 - (bs_data.r-bs_data.q)*bs_data.Smax/(2*dS))
+                BC_Ham += upperLeftHam(num_qbits) * -(bs_data.r + (bs_data.sigma*bs_data.Smin/dS)**2)
+                BC_Ham += upperLeft1Ham(num_qbits) * ((bs_data.sigma*bs_data.Smin/dS)**2/2 + (bs_data.r-bs_data.q)*bs_data.Smin/(2*dS))
+                BSHam += BC_Ham
+            case BoundaryConditions.DOUBLE_LINEAR:
+                BC_Ham = lowerRightHam(num_qbits) * -((bs_data.sigma*bs_data.Smax/dS)**2 + (bs_data.r-bs_data.q)*bs_data.Smax/dS)
+                BC_Ham += lowerRight1Ham(num_qbits) * ((bs_data.sigma*bs_data.Smax/dS)**2 / 2 + (bs_data.r-bs_data.q)*bs_data.Smax/(2*dS))
+                BC_Ham += upperLeftHam(num_qbits) * (-(bs_data.sigma*bs_data.Smin/dS)**2 + (bs_data.r-bs_data.q)*bs_data.Smin/dS)
+                BC_Ham += upperLeft1Ham(num_qbits) * ((bs_data.sigma*bs_data.Smin/dS)**2/2 - (bs_data.r-bs_data.q)*bs_data.Smin/dS)
                 BSHam += BC_Ham
             case _:
                 raise NotImplementedError('These boundary conditions are not yet implemented!')
