91b0708f7a89622e1b92ef62572d06e8ce79a5ba
Swagat NTU Office <swagat.ks.97@gmail.com>
2024-05-29 15:24:39 +0100

    added more option payoffs

--- tests/black_scholes/option_models.py
+++ tests/black_scholes/option_models.py
@@ -22,3 +22,54 @@ def EuropeanPutFormula(S:float, tau:float, strike:float, r:float, sigma:float):
     if tau == 0.0:
         return np.max([strike-S,0.0])
     return strike*np.exp(-r*tau) - S + EuropeanCallFormula(S,tau,strike,r,sigma)
+
+def BullSpreadPayoff(Smin:float,Smax:float,
+                     K1:float,K2:float,
+                     num_qbits:int) -> np.ndarray[float]:
+    # assert K2 > K1
+    Kmin = min(K1,K2)
+    Kmax = max(K1,K2)
+    return EuropeanCallState(Smin,Smax,Kmin,num_qbits) - EuropeanCallState(Smin,Smax,Kmax,num_qbits)
+
+def BearSpreadPayoff(Smin:float,Smax:float,
+                     K1:float,K2:float,
+                     num_qbits:int) -> np.ndarray[float]:
+    # assert K2 > K1
+    Kmin = min(K1,K2)
+    Kmax = max(K1,K2)
+    return EuropeanPutState(Smin,Smax,Kmax,num_qbits) - EuropeanPutState(Smin,Smax,Kmin,num_qbits)
+
+def StraddlePayoff(Smin:float,Smax:float,
+                   K:float,num_qbits:int) ->np.ndarray[float]:
+    return EuropeanCallState(Smin,Smax,K,num_qbits) + EuropeanPutState(Smin,Smax,K,num_qbits)
+
+def StranglePayoff(Smin:float,Smax:float,
+                   K1:float,K2:float,
+                   num_qbits:int) -> np.ndarray[float]:
+    Kmin = min(K1,K2)
+    Kmax = max(K1,K2)
+
+    return EuropeanPutState(Smin,Smax,Kmin,num_qbits) + EuropeanCallState(Smin,Smax,Kmax,num_qbits)
+
+def ButterflyPayoff(Smin:float,Smax:float,
+                    K:float,a:float,
+                    num_qbits:int) -> np.ndarray[float]:
+    return np.sum(
+        [
+        EuropeanCallState(Smin,Smax,K-a,num_qbits),
+        -2*EuropeanCallState(Smin,Smax,K,num_qbits),
+        EuropeanCallState(Smin,Smax,K+a,num_qbits),
+        ], axis=0)
+
+def CondorPayoff(Smin:float,Smax:float,
+                 K1:float,K2:float,a:float,
+                 num_qbits:int) -> np.ndarray[float]:
+    Kmin = min(K1,K2)
+    Kmax = max(K1,K2)
+    return np.sum(
+        [
+            EuropeanCallState(Smin,Smax,Kmin,num_qbits),
+            -EuropeanCallState(Smin,Smax,Kmin+a,num_qbits),
+            -EuropeanCallState(Smin,Smax,Kmax,num_qbits),
+            EuropeanCallState(Smin,Smax,Kmax+a,num_qbits),
+        ],axis=0)
